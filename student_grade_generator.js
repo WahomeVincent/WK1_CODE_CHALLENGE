6b36cfb984a804973c057a433820189fadf91b3d
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-20 23:13:51 +0300

    My student grade generator solution

---
+++ student_grade_generator.js
@@ -0,0 +1,13 @@
+let marks = 39;
+
+if (marks > 79) {
+    console.log("Your grade is : A ");
+} else if (marks >= 60 && marks <=79) {
+    console.log("Your grade is : B- ");
+} else if (marks >=49 && marks <=59) {
+    console.log("Your grade is : C- ");
+} else if (marks >=40 && marks <=49) {
+    console.log("Your grade is : D- ");
+} else if (marks < 40) {
+    console.log("Your grade is : E ");
+}
