3d3e57244c9f16e15d720a1265d252e0c4c1751a
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-20 23:18:57 +0300

    Solution

---
+++ speed_detector.js
@@ -0,0 +1,13 @@
+
+let speed = parseInt(prompt("Enter the speed of the car: "));
+
+if (speed < 70) {
+  console.log("Ok");
+} else {
+  let demeritPoints = Math.floor((speed - 70) / 5);
+  if (demeritPoints > 12) {
+    console.log("License suspended");
+  } else {
+    console.log(`Points: ${demeritPoints}`);
+  }
+}
