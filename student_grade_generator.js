2e656f57cbd5d5521f2dc945d20eba6db0aa3a47
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-23 10:10:34 +0300

    Completed

--- student_grade_generator.js
+++ student_grade_generator.js
@@ -1,15 +1,17 @@
 let marks = parseInt(prompt("Please enter the student's marks (between 0 and 100):"));
+const button = document.getElementById("grade");
 
-
-if (marks > 79) {
-    console.log("Your grade is : A ");
-} else if (marks >= 60 && marks <=79) {
-    console.log("Your grade is : B- ");
-} else if (marks >=49 && marks <=59) {
-    console.log("Your grade is : C- ");
-} else if (marks >=40 && marks <=49) {
-    console.log("Your grade is : D- ");
-} else if (marks < 40) {
-    console.log("Your grade is : E ");
-}
+button.addEventListener("click", function() {
+    if (marks > 79) {
+        return("Your grade is : A ");
+    } else if (marks >= 60 && marks <=79) {
+        return("Your grade is : B- ");
+    } else if (marks >=49 && marks <=59) {
+        return("Your grade is : C- ");
+    } else if (marks >=40 && marks <=49) {
+        return("Your grade is : D- ");
+    } else if (marks < 40) {
+        return("Your grade is : E ");
+    }
+  });
 
