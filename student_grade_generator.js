75f08f72680ecadc87886d3c408acd2bc04e776f
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-23 10:22:22 +0300

    Updated with comments

--- student_grade_generator.js
+++ student_grade_generator.js
@@ -1,6 +1,10 @@
+//Declares a variable 'marks' where to store the student grades.
+//ParseInt is used to ensure that the value passed is converted to an integer.
 let marks = parseInt(prompt("Please enter the student's marks (between 0 and 100):"));
 const button = document.getElementById("grade");
 
+
+//Adds an event listener to the button to display various statements if certain conditions are met.
 button.addEventListener("click", function() {
     if (marks > 79) {
         return("Your grade is : A ");
